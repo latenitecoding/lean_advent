6d1b8ed075a321899a0decef4d6a0913eae060c8
latenitecoding <latenitecode@gmail.com>
2022-12-06 11:41:21 -0600

    polishes solution

--- Advent/Advent2022/Day01.lean
+++ Advent/Advent2022/Day01.lean
@@ -1,4 +1,3 @@
-
 def Day01.parseInput (input : String) : Array Nat := Id.run do
   input
     |>.splitOn "\n\n"
@@ -23,21 +22,21 @@ Elf's inventory (if any) by a blank line.
 PART 1 : Find the Elf carrying the most Calories. How many total Calories
 is that Elf carrying?
 -/
-def Day01.part1 (input : String) : String := Id.run do
-  let maxElf ← Day01.parseInput input
+def Day01.part1 (input : String) : String :=
+  Day01.parseInput input
     |>.foldl (fun a b : Nat => if a ≥ b then a else b) 0
-  s!"{maxElf}"
+    |>.repr
 
 /-
 PART 2 : Find the top three Elves carrying the most Calories. How many
 Calories are those Elves carrying in total?
 -/
-def Day01.part2 (input : String) : String := Id.run do
-  let maxElvesSum ← Day01.parseInput input
+def Day01.part2 (input : String) : String :=
+  Day01.parseInput input
     |>.qsort (· ≥ ·)
     |>.extract 0 3
     |>.foldl (fun a b : Nat => a + b) 0
-  s!"{maxElvesSum}"
+    |>.repr
 
 def Day01.solve : IO (String × String) := do
   let input ← IO.FS.readFile ⟨"inputs/y2022d01.txt"⟩
